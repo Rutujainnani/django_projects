2f1858e9ffaf3cbab671d0b54dab3a3eb2fd00e4
Rutujainnani <ritujainnani22@gmail.com>
2024-05-14 11:44:22 +0530

    intial project uploaded

---
+++ ecom/product/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render,redirect
+from product.models import ProductTable
+
+
+# Create your views here.
+def home(request):
+   data={}
+   products=ProductTable.objects.filter(is_avaliable=True)
+   data['products']=products
+   return render(request,'base.html',context=data)
+
+def add_product(request):
+   if request.method=='POST':
+      name=request.POST.get('name')
+      price=request.POST.get('price')
+      description=request.POST.get('description')
+      quantity=request.POST.get('quantity')
+      category=request.POST.get('category')
+      image = request.FILES.get('image')
+      is_available=(request.POST.get('is_available',False)) and ('is_available' in request.POST)
+      
+      product=ProductTable.objects.create(name=name,price=price,description=description,quantity=quantity,category=category,image=image,is_available=is_available)
+      
+      product.save()
+      
+      return redirect("/admin/product/view/")
+   return render(request,'admin/product/add_product.html')
+
+
+def view_products(request):
+   data={}
+   products=ProductTable.objects.all()
+   data['products']=products
+   
+   return render(request,'admin/product/view_product.html',context=data)
+
+
+def delete_product(request,product_id):
+   product=ProductTable.objects.get(id=product_id)
+   product.delete()
+   return redirect("/admin/product/view/")
